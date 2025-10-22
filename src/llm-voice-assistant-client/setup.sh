e3dcc02c16d7f2d6f1b324553c10ca3f91771d5b
WesleyFister <flipperdipper506@gmail.com>
2025-10-21 20:10:56 -0700

    Simplify setup.sh

--- src/llm-voice-assistant-client/setup.sh
+++ src/llm-voice-assistant-client/setup.sh
@@ -1,24 +1,6 @@
-if [[ -z $(command -v ffmpeg) ]]; then
-    sudo apt install ffmpeg -y
-fi
-
-if [ -x "$(command -v apt-get)" ]; then
-    sudo apt-get install python3-venv -y
-    sudo apt-get install python3-pip -y
-    sudo apt-get install portaudio19-dev -y
-    sudo apt-get install libpython3-dev -y
-    sudo apt-get install pyenv -y
-elif [ -x "$(command -v pacman)" ]; then
-    sudo pacman -Sy pyenv
-fi
-
 pyenv install 3.11 --skip-existing
 pyenv global 3.11.14
 pyenv exec python3.11 -m venv venv
 
 source venv/bin/activate
 pip install -r requirements.txt
-
-# If not using Pulseaudio
-# sudo apt install pulseaudio -y
-# reboot
